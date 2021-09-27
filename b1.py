274ec9806e02e8f1608de8d1ad313edcc49fa8f0
vphnguyen <48557263+vphnguyen@users.noreply.github.com>
2021-09-27 15:25:10 +0700

    Nguyen

--- b1.py
+++ b1.py
@@ -6,35 +6,38 @@ import random
 outfile =open ('solieu.txt','w')
 row_col={"row":random.randint(2,10),"column":random.randint(2,10)}
 
-#=== Ghi file
+#=== Tao so và ghi vao file
 temp=[]
 for j in range(0,row_col['row']):
 	line=""
 	temp.clear()
 	for i in  range(0,row_col['column']):
+		#random , chuyen thanh STRING va them vao list
 		temp.append(str(random.randint(0,1000)))
-	line=" ".join(temp)
+	# Join lai bang dau cach
+	line=" ".join(temp) 
 	print(line)
 	print(line,file=outfile)
 print("Generated Done")
 outfile.close()
 
-#=== Doc file
+#=== Doc file, cong va ghi vao file moi
 infile =open ('solieu.txt','r')
 outfile =open ('linetotal.txt','w')
 array_in= []
 for line in infile:
 	array_in.clear()
+	# Doc line trong file, xoa endline "\n", tach String dau " " ra thanh nhieu array
 	array_in= line.rstrip().split(" ")
 	sum=0
 	for i in array_in:
+		# Cong trong array lai...
 		sum+= int(i)
+	# Ghi ket qua ra file...
 	print( "Dong"+str(array_in)+" co tong la: "+ str(sum),file=outfile) 
 print("Processing Done")
 infile.close()
 outfile.close()
-#===
-
 
 #===================================== Bai mau 1
 infile =open ('file.txt','r')
